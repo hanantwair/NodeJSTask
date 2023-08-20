c2d86529f20f56eee5d85baf97c2e7f982d59318
Hanan Twair <hanantwair168@gmail.com>
2023-08-21 01:38:35 +0300

    Modified NodeJS Task

--- front.js
+++ front.js
@@ -1,23 +1,40 @@
-function getImage() {
-    let name = document.getElementById("fileName").value;
-    if (name.length === 0) {
-        alert('Please Type a File Name');
-    }
-    else {
-        window.location.href = `/api/files/${name}`;
+async function uploadImage() {
+    const files = document.getElementById("file").files;
+    if (files.length) {
+        const formData = new FormData();
+        formData.append('file', files[0]);
+        if (validateFile(files[0])) {
+            const response = await fetch('/api/upload', {
+                method: "POST",
+                body: formData
+            });
+            const text = await response.text();
+            window.alert(text);
+        }
+    } else {
+        window.alert('No files!');
     }
 }
 
-function deleteImage() {
-    let name = document.getElementById("fileName").value;
-    if (name.length === 0) {
-        alert('Please Type a File Name');
-    } else {
-        fetch(`/api/files/delete/${encodeURIComponent(name)}`, {
-            method: 'DELETE',
-        })
-            .then(response => response.text()).then(message => { document.documentElement.innerHTML = message; })
-            .catch(err => { alert('Error: ' + err); });
-        // window.location.href = `/api/files/delete/${name}`;
+function validateFile(file) {
+    const fileExtension = ["image/jpeg", "image/png", "image/webp", "image/gif", "image/svg", "image/apng", "image/avif"];
+    if (fileExtension.includes(file.type)) {
+        if (file.size >= 8500 && file.size <= 114000) {
+            return true;
+        }
+        window.alert('file size must be between 8661 and 113906');
+        return false;
     }
+    window.alert('file type must be one of [jpeg, gif, png, ...]');
+    return false;
+}
+
+async function deleteImage(imgName) {
+    const response = await fetch(`/api/files/delete/${imgName}`, {
+        method: "POST"
+    });
+    const text = await response.text();
+    window.alert(text);
+    window.location.reload();
+
 }
--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(fileUpload());
 app.use(express.json());
@@ -14,7 +15,10 @@ mongoose.connect('mongodb://127.0.0.1/NodeJS', { useNewUrlParser: true, useUnifi
     .catch(err => console.log('Could not connect to MongoDB...', err));
 
 const filesSchema = new mongoose.Schema({
-    fileName: String,
+    fileName: {
+        type: String,
+        unique: true
+    },
     fileSize: Number,
     fileType: String
 });
@@ -22,39 +26,32 @@ const filesSchema = new mongoose.Schema({
 const File = mongoose.model('File', filesSchema);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('pages/index');
 });
 
 app.post('/api/upload', (req, res) => {
-    if (req.files) {
-        let file = req.files.file;
-        let fileName = file.name;
-        let fileSize = file.size;
-        let fileType = file.mimetype;
+    if (req.files.file) {
 
-        if (validateFile(file)) {
-            file.mv('./uploads/' + fileName, async (err) => {
-                if (err) throw err;
-                else {
-                    const file = new File({
-                        fileName: fileName,
-                        fileType: fileType,
-                        fileSize: fileSize,
-                    });
-                    try {
-                        const result = await file.save();
-                        console.log(result);
-                        res.send('File Uploaded!!');
-                    }
-                    catch (ex) {
-                        console.log(ex.message);
-                    }
+        let file = req.files.file;
+
+        file.mv('./uploads/' + file.name, async (err) => {
+            if (err) throw err;
+            else {
+                const fileObj = new File({
+                    fileName: file.name,
+                    fileType: file.mimetype,
+                    fileSize: file.size,
+                });
+                try {
+                    const result = await fileObj.save();
+                    res.send('File Uploaded!!');
                 }
-            });
-        }
-    }
-    else {
-        res.send('No files!');
+                catch (err) {
+                    console.log(err.message);
+                }
+            }
+        });
+
     }
 });
 
@@ -63,13 +60,24 @@ app.get('/api/files/:filename', async (req, res) => {
     const file = await File
         .find({ fileName: fileName });
     if (file && file.length) {
-        res.status(200).sendFile(__dirname + `/uploads/${fileName}`);
+        res.render('pages/img', {
+            fileName
+        });
     } else {
         res.status(404).send("This file doesn't exist!!");
     }
 });
 
-app.delete('/api/files/delete/:filename', async (req, res) => {
+app.get('/api/file/:filename', async (req, res) => {
+    const fileName = req.params.filename;
+    try {
+        res.status(200).sendFile(__dirname + `/uploads/${fileName}`);
+    } catch (err) {
+        res.status(404).send("This file doesn't exist!!");
+    }
+});
+
+app.post('/api/files/delete/:filename', async (req, res) => {
     const fileName = req.params.filename;
     const file = await File
         .findOneAndRemove({ fileName: fileName });
@@ -82,19 +90,19 @@ app.delete('/api/files/delete/:filename', async (req, res) => {
     }
 });
 
-const fileExtension = ["image/jpeg", "image/png", "image/webp", "image/gif", "image/svg", "image/apng", "image/avif"];
-
-function validateFile(file) {
-    if (fileExtension.includes(file.mimetype)) {
-        if (file.size >= 8661 && file.size <= 113906) {
-            return true;
-        }
-        console.log('file size must be between 8661 and 113906');
-        return false;
+app.get('/api/allFiles', async (req, res) => {
+    try {
+        const files = await File
+            .find()
+            .select({ fileName: 1, _id: 0 });
+        res.render('pages/allFiles', {
+            files
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error | ' + err.message });
     }
-    console.log('file type must be one of [jpeg, gif, png, ...]');
-    return false;
-}
+});
 
 const port = 3500 || process.env.PORT;
 app.listen(port, () => console.log(`Listening on ${port}...`));
